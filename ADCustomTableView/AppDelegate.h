4fdf12361ebccdb7cf5a10d8eaefb2160510920b
Anton Domashnev <antondomashnev@gmail.com>
2013-06-11 16:30:01 +0400

    Initial Commit

---
+++ ADCustomTableView/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ADCustomTableView
+//
+//  Created by Anton Domashnev on 11.06.13.
+//  Copyright (c) 2013 Anton Domashnev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
