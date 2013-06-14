6d94f0f34925ca3748c8948d053451b631595711
Антон Домашнев <antondomasnev@iMac-Anton.local>
2013-06-14 23:55:36 +0400

    Initial commit;

--- ADCustomTableView/ViewController.m
+++ ADCustomTableView/ViewController.m
@@ -7,9 +7,13 @@
 //
 
 #import "ViewController.h"
+#import "ADCustomTableView.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDataSource,
+                             UITableViewDelegate>
 
+@property (nonatomic, strong) NSMutableArray *dataArray;
+@property (nonatomic, strong) ADCustomTableView *tableView;
 @end
 
 @implementation ViewController
@@ -17,13 +21,57 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+    self.tableView = [[ADCustomTableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    self.tableView.autoresizingMask = UIViewAutoresizingFlexibleHeight;
+    [self.view addSubview: self.tableView];
+    
+    self.dataArray = [NSMutableArray array];
+    
+    for(int i = 0; i < 51; i++){
+        
+        [self.dataArray addObject: [NSString stringWithFormat:@"cell %d", i]];
+    }
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark UITableViewDataSource
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    return 44.;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+    return [self.dataArray count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    NSString *cellIdentifier = @"cellIdentifier";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier: cellIdentifier];
+    
+    if(!cell){
+        
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
+    }
+    
+    cell.textLabel.text = self.dataArray[indexPath.row];
+    
+    return cell;
 }
 
 @end
